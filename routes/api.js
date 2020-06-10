a904158ad4383ac5e54ce42c8f413d577c6dc19a
Tyler Jones <tjjones93@gmail.com>
2020-06-10 14:53:59 -0400

    Connect server to database
    Connect server to database in server.js but unable to connect to server through route in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,24 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
+/* reading from json file in project
 let jsonData = {};
 fs.readFile('bibliography.json', 'utf-8', (err, data) => {
     if (err) throw err
 
     jsonData = JSON.parse(data);
 });
+*/
 
+var db;
 
 router.get('/', (req, res) => {
-    return res.send(jsonData);
+    db.collection('books').find({}).toArray((err, books) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(books);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
-const books = require('./routes/api');
+
+// const books = require('./routes/api');
+
 
 // Create express app
 const app = express();
@@ -12,12 +15,37 @@ const port = process.env.PORT || 3000;
 
 // For API security
 app.use(helmet());
-app.use(bodyParser.json());
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Database connection
+const url = 'mongodb://localhost:27017';
+
+
+/*
 // Passes the /books route to the app
 app.use('/books', books);
+*/
+var db;
+app.get('/books', (req, res) => {
+    db.collection('books').find({}).toArray((err, books) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.send(books);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}. So it goes.`);
+
+    MongoClient.connect(url, function (err, client) {
+        // In mongo v.3.0.0+, you use client.db('dbName') to connect to your database
+        db = client.db('vonnegut');
+
+    });
+
 });
