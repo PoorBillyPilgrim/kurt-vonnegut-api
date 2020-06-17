c838d22c672084c25d2e51db8d4e113b2cd8ce92
Tyler Jones <tjjones93@gmail.com>
2020-06-17 18:10:32 -0400

    change route to novels

--- routes/api.js
+++ routes/api.js
@@ -1,49 +1,39 @@
 const express = require('express');
 const router = express.Router();
-// const fs = require('fs');
 const { catchErrors } = require('../handlers/errors.js');
 
-/* reading from json file in project
-let jsonData = {};
-fs.readFile('bibliography.json', 'utf-8', (err, data) => {
-    if (err) throw err
-
-    jsonData = JSON.parse(data);
-});
-*/
-
 // Passes the /books route to the app
-router.get('/books', (req, res) => {
+router.get('/novels', async (req, res) => {
     // req.app holds a reference to an instance of the Express application that is using the middleware
     // So app.locals.collection can be accessed in api.js using req.
     const collection = req.app.locals.collection;
-    /**
-     * I get an error when I have both res.send() for title and for all.
-     *     if (req.query.title) {
-        collection.find({ "title": req.query.title }).toArray((err, book) => {
+
+    if (req.query.title) {
+        /*await collection.find({ "title": req.query.title }).toArray((err, novel) => {
             if (err) {
                 res.status(500).send(err);
             }
-            res.send(book);
+            res.send(novel);
+        });*/
+        console.log(req.query.titlec)
+    } else {
+        await collection.find({}).toArray((err, novels) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.send(novels);
         });
     }
-     * 
-     */
-
-    collection.find({}).toArray((err, books) => {
-        if (err) {
-            res.status(500).send(err);
-        }
-        res.send(books);
-
-    });
 });
 
 // Returns document by title
-router.get('/books/:title', catchErrors(async (req, res, next) => {
+router.get('/novels/:title', catchErrors(async (req, res, next) => {
     const collection = req.app.locals.collection;
-    collection.find({ "title": req.params.title }).toArray((book) => {
-        res.send(book);
+    collection.find({ "title": req.params.title }).toArray((err, novel) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.send(novel);
     })
 }));
 
