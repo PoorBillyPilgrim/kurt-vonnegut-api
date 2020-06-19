a76ddeb522b58a239d410b93be50944601b75ddb
Tyler Jones <tjjones93@gmail.com>
2020-06-19 18:03:12 -0400

    add query handling

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,7 @@ router.get('/', async (req, res) => {
 router.get('/novels', async (req, res) => {
     // req.app holds a reference to an instance of the Express application that is using the middleware
     // So app.locals.collection can be accessed in api.js using req.
+    //const query = new URLSearchParams(req.query);
     const collection = req.app.locals.collection;
 
     await collection.find({ form: "novel" }).toArray((err, novels) => {
@@ -25,6 +26,11 @@ router.get('/novels', async (req, res) => {
         }
         res.send(novels);
     });
+    const { title, year } = req.query;
+    console.log(title);
+    console.log(parseInt(year));
+
+
 });
 
 // Returns document by title
@@ -35,7 +41,7 @@ router.get('/novels/:title', async (req, res) => {
             res.status(500).send(err);
         }
         if (!novel.length) {
-            res.status(404).json({ "error": "resource not found" })
+            res.status(404).json({ "error": "Sorry! Can't find that resource!" });
         } else {
             res.send(novel);
         }
--- server.js
+++ server.js
@@ -17,10 +17,12 @@ const port = process.env.PORT || 3000;
 app.use(helmet());
 app.use(cors());
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+// Query parser
 
 // Database connection
 const url = 'mongodb://localhost:27017';
